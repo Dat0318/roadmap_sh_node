1ab64a2812f1d9e05afd18923155f1b1cd60d768
dattd3 <dattd3@vingroup.net>
2023-09-18 17:23:23 +0700

    recurse menu

---
+++ data.js
@@ -0,0 +1,241 @@
+export const data = [
+  {
+    GroupName: '{vi:"TINH THẦN THÁI ĐỘ",en:"TINH THẦN THÁI ĐỘ"}',
+    GroupWeight: '10',
+    GroupOrder: 0,
+    CaculateType: 'SO',
+    Configs: [
+      {
+        Weight: '0% - 10%',
+        Description: '{vi:"Ít khi thể hiện",en:"Ít khi thể hiện"}',
+      },
+      {
+        Weight: '0% - 10%',
+        Description: '{vi:"Ít khi thể hiện",en:"Ít khi thể hiện"}',
+      },
+      {
+        Weight: '0% - 10%',
+        Description: '{vi:"Ít khi thể hiện",en:"Ít khi thể hiện"}',
+      },
+    ],
+    // FormConfigChild: [
+    //   {
+    //     GroupName: '{vi:"",en:""}',
+    //     GroupWeight: 0,
+    //     GroupOrder: 0,
+    //     CaculateType: null,
+    //     Configs: null,
+    //     Targets: [
+    //       {
+    //         TargetName:
+    //           '{vi:"1.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn",en:"1.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn"}',
+    //         Weight: '30',
+    //         Order: 0,
+    //       },
+    //       {
+    //         TargetName:
+    //           '{vi:"2.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn",en:"2.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn"}',
+    //         Weight: '30',
+    //         Order: 1,
+    //       },
+    //       {
+    //         TargetName:
+    //           '{vi:"3.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn",en:"3.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn"}',
+    //         Weight: '40',
+    //         Order: 2,
+    //       },
+    //     ],
+    //     FormConfigChild: [
+    //       {
+    //         GroupName: '{vi:"",en:""}',
+    //         GroupWeight: 0,
+    //         GroupOrder: 0,
+    //         CaculateType: null,
+    //         Configs: null,
+    //         Targets: [
+    //           {
+    //             TargetName:
+    //               '{vi:"1.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn",en:"1.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn"}',
+    //             Weight: '30',
+    //             Order: 0,
+    //           },
+    //           {
+    //             TargetName:
+    //               '{vi:"2.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn",en:"2.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn"}',
+    //             Weight: '30',
+    //             Order: 1,
+    //           },
+    //           {
+    //             TargetName:
+    //               '{vi:"3.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn",en:"3.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn"}',
+    //             Weight: '40',
+    //             Order: 2,
+    //           },
+    //         ],
+    //         FormConfigChild: [
+    //           {
+    //             GroupName: '{vi:"klvalnva",en:""}',
+    //             GroupWeight: 0,
+    //             GroupOrder: 0,
+    //             CaculateType: null,
+    //             Configs: null,
+    //             Targets: [
+    //               {
+    //                 TargetName:
+    //                   '{vi:"1.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn",en:"1.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn"}',
+    //                 Weight: '30',
+    //                 Order: 0,
+    //               },
+    //               {
+    //                 TargetName:
+    //                   '{vi:"2.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn",en:"2.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn"}',
+    //                 Weight: '30',
+    //                 Order: 1,
+    //               },
+    //               {
+    //                 TargetName:
+    //                   '{vi:"3.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn",en:"3.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn"}',
+    //                 Weight: '40',
+    //                 Order: 2,
+    //               },
+    //             ],
+    //             FormConfigChild: [
+    //               {
+    //                 GroupName: '{vi:"klvalnva",en:""}',
+    //                 GroupWeight: 0,
+    //                 GroupOrder: 0,
+    //                 CaculateType: null,
+    //                 Configs: null,
+    //                 Targets: [
+    //                   {
+    //                     TargetName:
+    //                       '{vi:"1.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn",en:"1.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn"}',
+    //                     Weight: '30',
+    //                     Order: 0,
+    //                   },
+    //                   {
+    //                     TargetName:
+    //                       '{vi:"2.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn",en:"2.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn"}',
+    //                     Weight: '30',
+    //                     Order: 1,
+    //                   },
+    //                   {
+    //                     TargetName:
+    //                       '{vi:"3.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn",en:"3.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn"}',
+    //                     Weight: '40',
+    //                     Order: 2,
+    //                   },
+    //                 ],
+    //                 FormConfigChild: [
+    //                   {
+    //                     GroupName: '{vi:"klvalnva",en:""}',
+    //                     GroupWeight: 0,
+    //                     GroupOrder: 0,
+    //                     CaculateType: null,
+    //                     Configs: null,
+    //                     Targets: [
+    //                       {
+    //                         TargetName:
+    //                           '{vi:"1.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn",en:"1.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn"}',
+    //                         Weight: '30',
+    //                         Order: 0,
+    //                       },
+    //                       {
+    //                         TargetName:
+    //                           '{vi:"2.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn",en:"2.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn"}',
+    //                         Weight: '30',
+    //                         Order: 1,
+    //                       },
+    //                       {
+    //                         TargetName:
+    //                           '{vi:"3.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn",en:"3.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn"}',
+    //                         Weight: '40',
+    //                         Order: 2,
+    //                       },
+    //                     ],
+    //                   },
+    //                 ],
+    //               },
+    //             ],
+    //           },
+    //         ],
+    //       },
+    //     ],
+    //   },
+    // ],
+    FormConfigChild: null,
+    Targets: null,
+  },
+  {
+    GroupName: '{vi:"KẾT QUẢ CÔNG VIỆC",en:"WORK RESULTS"}',
+    GroupWeight: '90',
+    GroupOrder: 1,
+    CaculateType: 'SO',
+    Configs: null,
+    FormConfigChild: [
+      {
+        GroupName: '{vi:"klvalnva",en:""}',
+        GroupWeight: 0,
+        GroupOrder: 0,
+        CaculateType: null,
+        Configs: null,
+        Targets: [
+          {
+            TargetName:
+              '{vi:"1.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn",en:"1.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn"}',
+            Weight: '30',
+            Order: 0,
+          },
+          {
+            TargetName:
+              '{vi:"2.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn",en:"2.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn"}',
+            Weight: '30',
+            Order: 1,
+          },
+          {
+            TargetName:
+              '{vi:"3.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn",en:"3.Luôn tuân thủ các nội quy, quy định, quy chế của Tập đoàn"}',
+            Weight: '40',
+            Order: 2,
+          },
+        ],
+      },
+    ],
+    Targets: null,
+  },
+];
+
+const demo = `
+const arr = [];
+const arr1, arr2, arr3;
+
+arr1 = {
+  id1: [{
+    // '...'
+  }]
+}
+
+arr2 = {
+  'id1.id1': {},
+  'id1.id2': {}
+}
+
+arr2 = {
+  'id1.id2': [
+    'id2.id1': {},
+    'id2.id2': {},
+  ]
+}
+
+arr3 = {
+  id1: [
+    id1.id2.id4: [
+
+    ]
+  ]
+}
+
+// indexCap, key
+
+[arr1, arr2, arr3, ...]
+`;
--- node.js
+++ node.js
@@ -1,1 +1,30 @@
-import fetch from 'node-fetch';
+import _ from 'lodash';
+
+import { data } from './data.js';
+
+// console.log('----------------------------------------');
+// console.log('data: ', data);
+// console.log('----------------------------------------');
+
+const arrRoot = [],
+  child_key = 'FormConfigChild';
+
+function recurse(arr, key = '') {
+  const children = [],
+    obj = {};
+  arr.forEach((ele, i) => {
+    children.push(_.omit(ele, [child_key]));
+
+    if (Array.isArray(ele[child_key])) {
+      recurse(ele[child_key], `${key}.${i}`);
+    }
+  });
+  obj[key] = children;
+  arrRoot.push(obj);
+}
+
+recurse(data);
+
+console.log('----------------------------------------');
+console.log('arrRoot: ', JSON.stringify(arrRoot, undefined, 2));
+console.log('----------------------------------------');
